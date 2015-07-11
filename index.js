2c693428b0999ae8643d525ecd6bf87dd327cee1
James Talmage <james@talmage.io>
2015-07-11 04:05:34 -0400

    remove unused sinopia test code

--- index.js
+++ index.js
@@ -10,8 +10,6 @@ function whoami(opts, cb) {
     cb = opts;
     opts = {};
   }
-  var timeout = opts.timeout || 10000;
-  assert('number', typeof timeout, 'timeout');
 
   which('npm', callNpm);
 
@@ -19,16 +17,29 @@ function whoami(opts, cb) {
     if (err) {
       return cb(err);
     }
-    var cmd = pathToNpm + ' whoami';
-    if (opts.registry) {
-      cmd += ' --registry ' + opts.registry;
-    }
-    exec(cmd, {encoding:'utf8', timeout:timeout}, handleResult);
+    var callConfig = makeCallConfig(opts, pathToNpm);
+    exec(callConfig.command, callConfig.execOpts, handleResult);
   }
 
   function handleResult(err, stdout, stderr) {
     var old = cb;
     cb = function() {};
     old(err, stdout && stdout.trim());
+  }
+}
+
+function makeCallConfig(opts, pathToNpm) {
+  opts = opts || {};
+  var timeout = opts.timeout || 10000;
+  assert('number', typeof timeout, 'timeout');
+
+  var cmd = pathToNpm + ' whoami';
+  if (opts.registry) {
+    cmd += ' --registry ' + opts.registry;
+  }
+
+  return {
+    command: cmd,
+    execOpts: {encoding: 'utf8', timeout: timeout}
   }
 }
