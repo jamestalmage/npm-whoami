6265fab7e9a6f5057ef7cab642938f290393bf06
James Talmage <james@talmage.io>
2015-07-11 02:26:52 -0400

    drop sinopia and just use express for testing

--- test/test.js
+++ test/test.js
@@ -1,22 +1,38 @@
 'use strict';
 var assert = require('assert');
 var npmWhoami = require('../');
-var sinopia = require('./lib/sinopia');
 var path = require('path');
 var proxyquire = require('proxyquire');
+var express = require('express');
 
 describe('npm-whoami', function() {
   this.timeout(10000);
   var originalDirectory;
+  var username;
+  var server;
+
+  var app = express();
+
+  app.get('/whoami', function(req, res) {
+    res.json({username:username});
+  });
+
+  app.get('/-/whoami', function(req, res) {
+    res.json({username:username});
+  });
 
   before(function(done) {
     originalDirectory = process.cwd();
-    sinopia.start(done);
+    server = app.listen(55550, done);
   });
 
   after(function(done) {
     process.chdir(originalDirectory);
-    sinopia.stop(done);
+    server.close(done);
+  });
+
+  beforeEach(function() {
+    username = undefined;
   });
 
   function chdir(name) {
@@ -25,6 +41,7 @@ describe('npm-whoami', function() {
 
   it('jane.doe', function(done) {
     chdir('jane');
+    username = 'jane.doe';
     npmWhoami({registry: 'http://localhost:55550'}, function(err, result) {
       assert.ifError(err);
       assert.equal(result, 'jane.doe');
@@ -34,6 +51,7 @@ describe('npm-whoami', function() {
 
   it('john.doe', function(done) {
     chdir('john');
+    username = 'john.doe';
     npmWhoami({registry: 'http://localhost:55550'}, function(err, result) {
       assert.ifError(err);
       assert.equal(result, 'john.doe');
@@ -41,11 +59,20 @@ describe('npm-whoami', function() {
     });
   });
 
-  it('unauth', function(done) {
+  it('unauth - bad return value', function(done) {
+    chdir('unauth');
+    npmWhoami({registry: 'http://localhost:55550'}, function(err, result) {
+      assert(err);
+      done();
+    });
+  });
+
+  it('unauth - not logged in', function(done) {
     chdir('unauth');
     npmWhoami(function(err, result) {
-      assert(err);
-      //assert.strictEqual(result, null);
+      if (!err) {
+        assert.fail('This test will fail if you are logged into npm: YOU ARE: '  + result);
+      }
       done();
     });
   });
